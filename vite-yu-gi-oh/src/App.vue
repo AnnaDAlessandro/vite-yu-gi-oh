f07f94813e0d28101d602d3b6b2ac4bcb1386f78
Anna <annadalex@icloud.com>
2024-02-13 17:30:04 +0100

    Non mi trova cards list

--- vite-yu-gi-oh/src/App.vue
+++ vite-yu-gi-oh/src/App.vue
@@ -1,13 +1,12 @@
 <script >
 import AppHeader from './components/AppHeader.vue';
-import AppMain from './components/AppMain.vue';
+import AppMain from './components/AppMain.vue'
 
 
 export default{
   components:{
           AppHeader,
-          AppMain,
-          CardsList
+          AppMain
   },
           data (){
        return{
